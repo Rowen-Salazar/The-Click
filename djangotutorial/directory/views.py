eafa81691e10c5c571d984b2d2a6fe2522f95fa8
Kallus878 <kallussydney88@gmail.com>
2025-02-22 01:22:31 -0600

    Nav Bar Home Page
    -created rough draft of the nav bar on the home page
    -all html and css for the nav bar design is located in home.html

--- djangotutorial/directory/views.py
+++ djangotutorial/directory/views.py
@@ -1,14 +1,15 @@
 
 from django.shortcuts import get_object_or_404, render
 from directory.models import Map
+from django.conf import settings
 #from django.http import HttpResponse
 
 def home(request):
     map_list = Map.objects.all()
     return render(request, 'home.html', {'map_list': map_list})
 
-def about(request):
-    return render(request, 'about.html')
+#def about(request):
+#    return render(request, 'about.html')
 
 def mapview(request, map_name):
     full_map = get_object_or_404(Map, name=map_name)
@@ -16,3 +17,6 @@ def mapview(request, map_name):
 
 def buildingview(request):
     return render(request, 'buildingview.html')
+
+def my_view(request):
+    return render(request, 'home.html', {'MEDIA_URL': settings.MEDIA_URL})
