d1bd8ce20e23dffe92bf675a6bfbcc1dddb609f9
Pixelmahn <rowensalazar06@gmail.com>
2025-02-28 15:37:06 -0600

    Page Updates
    - Map object callable on home page
    - Map and Filter object callable on mapview page
    - Building object callable on buildinglist (buildingview/) page
    - Building and Filter object callable on buildingview page
    - Added base Location object in admin panel
    - Added buildinglist view as a default for buildingview/
    - Linked Filter and Location objects

--- djangotutorial/directory/admin.py
+++ djangotutorial/directory/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Map, Building, Filter
+from .models import Map, Building, Filter, Location
 
 admin.site.register(Map)
 admin.site.register(Building)
 admin.site.register(Filter)
+admin.site.register(Location)
 
 # Register your models here.
--- djangotutorial/directory/views.py
+++ djangotutorial/directory/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import get_object_or_404, render
-from directory.models import Map
+from directory.models import Map, Building, Filter, Location
 from django.conf import settings
 #from django.http import HttpResponse
 
@@ -8,15 +8,27 @@ def home(request):
     map_list = Map.objects.all()
     return render(request, 'home.html', {'map_list': map_list})
 
-#def about(request):
-#    return render(request, 'about.html')
-
 def mapview(request, map_name):
     full_map = get_object_or_404(Map, name=map_name)
-    return render(request, 'mapview.html', {'full_map': full_map})
+    all_filters = Filter.objects.all()
+    context = {
+        'full_map': full_map,
+        'all_filters': all_filters
+    }
+    return render(request, 'mapview.html', context)
 
-def buildingview(request):
-    return render(request, 'buildingview.html')
+def buildinglist(request):
+    building_list = Building.objects.all()
+    return render(request, 'buildingview.html', {'building_list': building_list})
+
+def buildingview(request, building_name):
+    full_building = get_object_or_404(Building, name=building_name)
+    all_filters = Filter.objects.all()
+    context = {
+        'full_building': full_building,
+        'all_filters': all_filters
+    }
+    return render(request, 'buildingview.html', context)
 
 def my_view(request):
     return render(request, 'home.html', {'MEDIA_URL': settings.MEDIA_URL})
