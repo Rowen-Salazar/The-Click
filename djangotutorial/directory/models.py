c9b18f6029d3c94b29bb64678934c757d9aa2adb
Pixelmahn <rowensalazar06@gmail.com>
2025-03-04 11:19:50 -0600

    Model Fixes
    Fixed the migrate issues

--- djangotutorial/directory/models.py
+++ djangotutorial/directory/models.py
@@ -2,25 +2,12 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-
-class Location(models.Model):
-    name = models.CharField(max_length=100)
-    display_name = models.CharField(max_length=200, blank=True)
-    coordinates = models.CharField(max_length=1000, blank=True)
-    def __str__(self):
-        return self.display_name
-    
-class Filter(models.Model):
-    name = models.CharField(max_length=100)
-    display_name = models.CharField(max_length=200, blank=True)
-    def __str__(self):
-        return self.name
     
 class Map(models.Model):
     name = models.CharField(max_length=50)
     display_name = models.CharField(max_length=200, blank=True)
-    #Placeholder for Google API Integration
     map_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    #Google API Stuff
     zoom = models.IntegerField(blank=False, default=17)
     center = models.CharField(max_length=1000, blank=True)
     map_id = models.CharField(max_length=1000, blank=True)
@@ -33,29 +20,41 @@ class Building(models.Model):
     floors = models.IntegerField(blank=False, default=1)
     floor_plan = models.ImageField(null=True, blank=True, upload_to="images/")
     on_map = models.ForeignKey(Map, on_delete=models.CASCADE)
-    
     #for sidebar
-    #address = models.CharField(max_length=225)
-    #image = models.ImageField(upload_to='images/buildingSearchBox/')
-    #slug = models.SlugField(unique=True)
+    address = models.CharField(max_length=225, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='images/buildingSearchBox/')
+    slug = models.SlugField(unique=True, null=True, blank=True)
     
     def __str__(self):
         return self.display_name
-
+    
 class Filter(models.Model):
-    name = models.CharField('Option Name', max_length=100)
-    onmaps = models.BooleanField('Included In Map Page?')
-    onbuildings = models.BooleanField('Included In Building Page?')
+    MAP = "Map"
+    BUILDING = "Building"
+    ACCOUNT_TYPE_CHOICES = {
+        MAP: "Map",
+        BUILDING: "Building",
+    }
+    name = models.CharField(max_length=100)
+    display_name = models.CharField(max_length=200, blank=True)
+    page_type = models.CharField(
+        max_length=100,
+        choices=ACCOUNT_TYPE_CHOICES,
+        default=MAP,
+    )
     def __str__(self):
         return self.name
-
+    
 class Location(models.Model):
-    name = models.CharField(max_length=500,blank=True, null=True)
+    name = models.CharField(max_length=100)
+    display_name = models.CharField(max_length=200, blank=True)
     zipcode = models.CharField(max_length=200,blank=True, null=True)
     city = models.CharField(max_length=200,blank=True, null=True)
     adress = models.CharField(max_length=200,blank=True, null=True)
     latitude = models.CharField(max_length=1000, blank=True)
     longitude = models.CharField(max_length=1000, blank=True)
-    filter_category = models.CharField(max_length=1000, blank=True)
+    filter_category = models.ForeignKey(Filter, on_delete=models.CASCADE, null=True)
+    #Optional if on a building on the building page
+    on_building = models.ForeignKey(Building, on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self):
-        return self.name
+        return self.display_name
